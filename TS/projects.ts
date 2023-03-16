3f179903a0c615dab008b44100abb740a0bd7679
Sofilus <sofia00olsson@gmail.com>
2023-03-16 16:31:43 +0100

    Added projects.ts

---
+++ TS/projects.ts
@@ -0,0 +1,6 @@
+export function createProjectCards (){
+    let projectContainer = document.querySelector(".projects-container-desktop") as HTMLElement;
+    let projectCard = document.createElement("div") as HTMLDivElement;
+    projectCard.classList.add('project-card');
+    projectContainer.appendChild(projectCard);
+};
